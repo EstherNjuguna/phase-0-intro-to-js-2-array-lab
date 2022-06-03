32c7f6aa493810c9ffa4c88793e83eb68f88c96c
+ <+>
2022-06-03 23:36:28 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function(){
+    cats.length=0;
+    cats.push("Milo" , "Otis" ,"Garfield");
+})
+function destructivelyAppendCat(name){
+    return(cats.push(name));
+}
+
+function destructivelyPrependCat(Bob) {
+    return (cats.unshift(Bob));
+}
+function  destructivelyRemoveLastCat(name){
+    return(cats.pop(name));
+}
+function  destructivelyRemoveFirstCat(name){
+    return(cats.shift(name));
+}
+
+function appendCat(name){
+    return[...cats,name]
+}
+
+
+function prependCat(name){
+    return[name,...cats];
+}
+function removeLastCat(){
+    return cats.slice(0, cats.length-1);
+}
+function removeFirstCat(){
+    return cats.slice(1);
+}
+    
+
